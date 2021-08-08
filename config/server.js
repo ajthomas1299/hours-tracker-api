db828660819e680f911f4ff162b4593c6ed3b2aa
Sven-27 <sven.notermans@live.nl>
2021-08-08 21:51:11 +0200

    setup backend

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b4b29f3f2b22837cc7f1a14834bb128c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '20f8897c-6dac-4249-aec4-6f6182db3b8c'
+};
